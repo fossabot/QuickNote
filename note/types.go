5a8b4fa42bbb68557bad8f68a5fe53a3c578c6db
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-05 01:03:34 +0800

    feat: note types

--- note/types.go
+++ note/types.go
@@ -1,13 +1,27 @@
 package note
 
-type Note struct {
-	ID string
+import (
+	"gorm.io/gorm"
+)
 
-	Title   []byte
-	Content []byte
+type Payload struct {
+	Title   []byte `gorm:"-"`
+	Content []byte `gorm:"-"`
+}
+
+type Note struct {
+	gorm.Model
+	NID  string `gorm:"uniqueIndex"`
+	Lock bool
+	Data []byte
+	Key  []byte `gorm:"-"`
+
+	Payload
 }
 
 type DisplayNote struct {
+	NID     string `json:"nid"`
+	Lock    bool   `json:"lock"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
