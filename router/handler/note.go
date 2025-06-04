3c2b391a145a8af205a330858de934ce8384612e
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-05 01:03:50 +0800

    feat: note route

--- router/handler/note.go
+++ router/handler/note.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"strings"
 
+	"github.com/Sn0wo2/QuickNote/helper"
 	"github.com/Sn0wo2/QuickNote/log"
 	"github.com/Sn0wo2/QuickNote/note"
 	"github.com/Sn0wo2/QuickNote/response"
@@ -11,57 +12,62 @@ import (
 	"go.uber.org/zap"
 )
 
-func Note(path string) func(c *fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		path := strings.TrimPrefix(strings.TrimPrefix(c.Path(), setup.APIVersion), path)
-		if path == "" {
-			return c.Status(fiber.StatusNotFound).JSON(response.New(false, "Note not found"))
+func Note(path string) func(ctx *fiber.Ctx) error {
+	return func(ctx *fiber.Ctx) error {
+		notePath := strings.TrimPrefix(strings.TrimPrefix(strings.TrimPrefix(ctx.Path(), setup.APIVersion), path), "/")
+		if notePath == "" {
+			return ctx.Status(fiber.StatusNotFound).JSON(response.New(false, "Note not found"))
 		}
-		n := note.Note{ID: path}
-		switch c.Method() {
-		case fiber.MethodPost:
-			body := c.Query("title")
-			if body == "" {
-				return c.Status(fiber.StatusBadRequest).JSON(response.New(false, "title is required"))
-			}
-			n.Title = []byte(body)
 
-			body = c.Query("content")
-			if body == "" {
-				return c.Status(fiber.StatusBadRequest).JSON(response.New(false, "content is required"))
+		n := note.Note{
+			NID: notePath,
+			Key: helper.StringToBytes(ctx.Query("key")),
+		}
+
+		switch ctx.Method() {
+		case fiber.MethodPost:
+			title := ctx.Query("title")
+			content := ctx.Query("content")
+
+			if title == "" || content == "" {
+				msg := "title is required"
+				if content == "" {
+					msg = "content is required"
+				}
+				return ctx.Status(fiber.StatusBadRequest).JSON(response.New(false, msg))
 			}
-			n.Content = []byte(body)
+
+			n.Title = helper.StringToBytes(title)
+			n.Content = helper.StringToBytes(content)
 
 			if err := n.Write(); err != nil {
-				return err
+				log.Instance.Error("Failed to write note", zap.Error(err), zap.String("ctx", ctx.String()))
+				return ctx.Status(fiber.StatusInternalServerError).JSON(response.New(false, "failed to write note"))
 			}
-			log.Instance.Info("Note created",
-				zap.String("path", path),
-				zap.String("ip", c.IP()),
-			)
-			return c.Status(fiber.StatusOK).JSON(response.New(true, "success"))
+
+			log.Instance.Info("Note created", zap.String("ctx", ctx.String()))
+			return ctx.Status(fiber.StatusOK).JSON(response.New(true, "success"))
+
 		case fiber.MethodDelete:
 			if err := n.Delete(); err != nil {
-				// log.Instance.Warn("Note not found", zap.Error(err))
-				return c.Status(fiber.StatusNotFound).JSON(response.New(false, "Note not found"))
+				log.Instance.Warn("Note not found", zap.Error(err), zap.String("ctx", ctx.String()))
+				return ctx.Status(fiber.StatusNotFound).JSON(response.New(false, err.Error()))
 			}
-			log.Instance.Info("Note deleted",
-				zap.String("path", path),
-				zap.String("ip", c.IP()),
-			)
-			return c.Status(fiber.StatusOK).JSON(response.New(true, "success"))
+
+			log.Instance.Info("Note deleted", zap.String("ctx", ctx.String()))
+			return ctx.Status(fiber.StatusOK).JSON(response.New(true, "success"))
+
 		default:
 			if err := n.Read(); err != nil {
-				// log.Instance.Warn("Note not found", zap.Error(err))
-				return c.Status(fiber.StatusNotFound).JSON(response.New(false, "Note not found"))
+				log.Instance.Warn("Note not found", zap.Error(err), zap.String("ctx", ctx.String()))
+				return ctx.Status(fiber.StatusNotFound).JSON(response.New(false, err.Error()))
 			}
-			/*log.Instance.Info("Note found",
-				zap.String("path", path),
-				zap.String("ip", c.IP()),
-			)*/
-			return c.Status(fiber.StatusOK).JSON(response.New(true, "success", note.DisplayNote{
-				Title:   string(n.Title),
-				Content: string(n.Content),
+
+			log.Instance.Info("Note found", zap.String("ctx", ctx.String()))
+			return ctx.Status(fiber.StatusOK).JSON(response.New(true, "success", note.DisplayNote{
+				NID:     n.NID,
+				Title:   helper.BytesToString(n.Title),
+				Content: helper.BytesToString(n.Content),
 			}))
 		}
 	}
