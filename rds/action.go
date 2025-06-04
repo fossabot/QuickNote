2784b94aa5dc9c4912626fdaff0252d9a27c8120
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-05 01:05:44 +0800

    feat: redis action

---
+++ rds/action.go
@@ -0,0 +1,35 @@
+package rds
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/Sn0wo2/QuickNote/compress"
+)
+
+func SetCompressedBytes(key string, value []byte, expire time.Duration) error {
+	if len(value) == 0 {
+		return errors.New("empty value")
+	}
+
+	value, err := compress.CompressFlate(value)
+	if err != nil {
+		return err
+	}
+
+	return Instance.Get().Set(context.Background(), key, value, expire).Err()
+}
+
+func GetCompressedBytes(key string) ([]byte, error) {
+	value, err := Instance.Get().Get(context.Background(), key).Bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	value, err = compress.DecompressFlate(value)
+	if err != nil {
+		return nil, err
+	}
+	return value, nil
+}
