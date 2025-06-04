bb4213487a8de46880a98533ef6435b9b8b6180b
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-05 01:05:07 +0800

    refactor: use sql store note

--- note/note.go
+++ note/note.go
@@ -1,10 +1,22 @@
 package note
 
-import (
-	"os"
-	"path/filepath"
-)
+func (n *Note) Write() error {
+	encoded, err := n.Encode(n.Key)
+	if err != nil {
+		return err
+	}
+	n.Data = encoded
+	return SetNote(*n)
+}
+
+func (n *Note) Read() error {
+	no, err := GetNote(n.NID)
+	if err != nil {
+		return err
+	}
+	return n.Decode(no.Data, n.Key)
+}
 
 func (n *Note) Delete() error {
-	return os.Remove(filepath.Join(".", n.ID))
+	return DeleteNote(n.NID)
 }
