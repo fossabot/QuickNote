196ea35de069ccdcd11f7fde01a2b1b7e22fbbc7
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-05 22:12:47 +0800

    fix: remove config redis

--- config/types.go
+++ config/types.go
@@ -1,10 +1,6 @@
 package config
 
 type Config struct {
-	Redis struct {
-		URL      string `optional:"true" yaml:"url"`
-		Password string `optional:"true" yaml:"password"`
-	} `yaml:"redis"`
 	Database struct {
 		Type string `yaml:"type"`
 		URL  string `yaml:"url"`
