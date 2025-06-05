b205036389a12307099ba865a5ed55e63d0300c9
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-05 22:13:01 +0800

    feat: static router

--- router/router.go
+++ router/router.go
@@ -1,6 +1,10 @@
 package router
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/Sn0wo2/QuickNote/log"
 	"github.com/Sn0wo2/QuickNote/response"
 	"github.com/Sn0wo2/QuickNote/router/handler"
@@ -16,19 +20,52 @@ func Setup(app *fiber.App) {
 	}))
 
 	api := app.Group(setup.APIVersion)
-
 	const healthPath = "health"
-
 	const notePath = "note"
 
 	api.Group(healthPath, handler.Health(healthPath))
 	api.Group(notePath, handler.Note(notePath))
 
-	app.Use("*", func(ctx *fiber.Ctx) error {
-		log.Instance.Warn("Route not found",
-			zap.String("ctx", ctx.String()),
-		)
+	api.Use("*", func(ctx *fiber.Ctx) error {
+		log.Instance.Warn("API route not found", zap.String("ctx", ctx.String()))
+		return ctx.Status(fiber.StatusNotFound).JSON(response.New(false, "API endpoint not found"))
+	})
 
-		return ctx.Status(fiber.StatusNotFound).JSON(response.New(false, "Route not found"))
+	const staticRoot = "./static"
+	app.Static("/", staticRoot, fiber.Static{
+		Compress:  true,
+		ByteRange: true,
+		Index:     "index.html",
+	})
+
+	app.Use("*", func(ctx *fiber.Ctx) error {
+		if ctx.Method() != fiber.MethodGet {
+			log.Instance.Warn("Global route not found", zap.String("ctx", ctx.String()))
+			return ctx.Status(fiber.StatusNotFound).JSON(response.New(false, "Resource not found"))
+		}
+
+		path := strings.TrimPrefix(ctx.Path(), "/")
+		base := filepath.Join(staticRoot, path)
+
+		for {
+			possibleIndex := filepath.Join(base, "index.html")
+			if _, err := os.Stat(possibleIndex); err == nil {
+				return ctx.SendFile(possibleIndex)
+			}
+
+			if base == staticRoot {
+				break
+			}
+
+			base = filepath.Dir(base)
+		}
+
+		defaultIndex := filepath.Join(staticRoot, "index.html")
+		if _, err := os.Stat(defaultIndex); err == nil {
+			return ctx.SendFile(defaultIndex)
+		}
+
+		log.Instance.Warn("Global route not found", zap.String("ctx", ctx.String()))
+		return ctx.Status(fiber.StatusNotFound).JSON(response.New(false, "Resource not found"))
 	})
 }
