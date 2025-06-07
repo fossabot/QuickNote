48f717d51a9d473a14460386433de5aac816f11d
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-08 05:32:45 +0800

    refactor: note marshal

--- note/marshal.go
+++ note/marshal.go
@@ -8,110 +8,97 @@ import (
 	"io"
 
 	"github.com/Sn0wo2/QuickNote/compress"
-	"github.com/Sn0wo2/QuickNote/helper"
 )
 
 const (
 	fieldTitle   = 1
 	fieldContent = 2
-	magicHeader  = "NOTE"
 	magicVersion = 1
 )
 
-func (n *Note) Encode() error {
-	var buf bytes.Buffer
+// magicHeader First 2 bytes of SHA-256 hash of "QuickNote"
+var magicHeader = [...]byte{0x34, 0x19}
 
-	buf.WriteString(magicHeader)
+func (n *Note) Encode() error {
+	if n == nil {
+		return errors.New("nil note")
+	}
+
+	buf := bytes.NewBuffer(make([]byte, 0, 256))
+	buf.Grow(len(magicHeader) + 1 + 1 + 4 + len(n.Title) + 1 + 4 + len(n.Content))
+
+	buf.Write(magicHeader[:])
 	buf.WriteByte(magicVersion)
 
-	for _, field := range []struct {
-		id   byte
-		data []byte
-	}{
-		{fieldTitle, n.Title},
-		{fieldContent, n.Content},
-	} {
-		if err := buf.WriteByte(field.id); err != nil {
-			return err
-		}
-
-		if err := binary.Write(&buf, binary.LittleEndian, uint32(len(field.data))); err != nil {
-			return err
-		}
-
-		if _, err := buf.Write(field.data); err != nil {
-			return err
-		}
+	writeField := func(id byte, data []byte) {
+		buf.WriteByte(id)
+		binary.Write(buf, binary.LittleEndian, uint32(len(data)))
+		buf.Write(data)
 	}
 
-	compressData, err := compress.FlateCompress(buf.Bytes())
-	if err != nil {
-		return err
-	}
+	writeField(fieldTitle, n.Title)
+	writeField(fieldContent, n.Content)
 
-	n.Data = compressData
-	n.Title = nil
-	n.Content = nil
+	var err error
+	n.Data, err = compress.FlateCompress(buf.Bytes())
+	n.Title, n.Content = nil, nil
 
-	return nil
+	return err
 }
 
 func (n *Note) Decode(data []byte) error {
-	var err error
-
-	data, err = compress.FlateDecompress(data)
-	if err != nil {
-		return fmt.Errorf("decompression failed: %w", err)
+	if n == nil {
+		return errors.New("nil note")
 	}
 
-	if !bytes.HasPrefix(data, helper.StringToBytes(magicHeader)) {
+	decompressed, err := compress.FlateDecompress(data)
+	if err != nil {
+		return fmt.Errorf("decompress: %w", err)
+	}
+
+	if len(decompressed) < len(magicHeader)+1 {
+		return errors.New("data too short")
+	}
+
+	if !bytes.Equal(decompressed[:len(magicHeader)], magicHeader[:]) {
 		return errors.New("invalid magic header")
 	}
 
-	r := bytes.NewReader(data[len(magicHeader):])
-
-	version, err := r.ReadByte()
-	if err != nil || version != magicVersion {
-		return fmt.Errorf("unsupported or invalid version: %d", version)
+	r := bytes.NewReader(decompressed[len(magicHeader):])
+	if v, err := r.ReadByte(); err != nil || v != magicVersion {
+		return fmt.Errorf("invalid version: %w", err)
 	}
 
-	defer func() {
-		n.Data = nil
-	}()
-
+	buf := make([]byte, 4)
 	for {
 		id, err := r.ReadByte()
 		if err == io.EOF {
 			break
 		}
-
 		if err != nil {
-			return err
+			return fmt.Errorf("read field id: %w", err)
 		}
 
-		var length uint32
-		if err := binary.Read(r, binary.LittleEndian, &length); err != nil {
-			return err
+		if _, err := io.ReadFull(r, buf); err != nil {
+			return fmt.Errorf("read field %d length: %w", id, err)
 		}
 
-		if int64(length) > int64(r.Len()) {
-			return fmt.Errorf("field %d length %d exceeds remaining %d", id, length, r.Len())
-		}
-
-		val := make([]byte, length)
-		if _, err = io.ReadFull(r, val); err != nil {
-			return err
+		length := binary.LittleEndian.Uint32(buf)
+		value := make([]byte, length)
+		if _, err := io.ReadFull(r, value); err != nil {
+			return fmt.Errorf("read field %d: %w", id, err)
 		}
 
 		switch id {
 		case fieldTitle:
-			n.Title = val
+			n.Title = value
 		case fieldContent:
-			n.Content = val
+			n.Content = value
 		default:
-			return fmt.Errorf("unknown field id: %d", id)
+			return fmt.Errorf("unknown field: %d", id)
 		}
 	}
 
+	n.Data = nil
 	return nil
 }
