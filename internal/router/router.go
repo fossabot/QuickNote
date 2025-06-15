ff0bcf25150c9b18711b74ef716d7a53bbaa8ecd
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-15 14:39:39 +0800

    feat: static and not found handler

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,81 +1,30 @@
 package router
 
 import (
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
-
 	"github.com/Sn0wo2/QuickNote/internal/router/handler"
+	"github.com/Sn0wo2/QuickNote/internal/router/notfound"
+	"github.com/Sn0wo2/QuickNote/internal/router/static"
 	"github.com/Sn0wo2/QuickNote/internal/setup"
-	"github.com/Sn0wo2/QuickNote/pkg/log"
-	"github.com/Sn0wo2/QuickNote/pkg/response"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"go.uber.org/zap"
 )
 
-func Setup(app *fiber.App) {
-	app.Use(compress.New(compress.Config{
+func Setup(router fiber.Router) {
+	router.Use(compress.New(compress.Config{
 		Level: compress.LevelBestSpeed,
 	}))
 
-	app.Use(cors.New())
+	router.Use(cors.New())
 
-	api := app.Group(setup.APIVersion)
+	api := router.Group(setup.APIVersion)
 
 	api.Group("health", handler.Health())
 	api.Group("notes/*", handler.Note())
 
-	api.Use("*", func(ctx *fiber.Ctx) error {
-		log.Instance.Warn("API route not found", zap.String("ctx", ctx.String()))
+	notfound.Setup("api not found", api)
 
-		return ctx.Status(fiber.StatusNotFound).JSON(response.New(false, "api endpoint not found"))
-	})
+	static.Setup(router)
 
-	const staticRoot = "./static"
-
-	app.Static("/", staticRoot, fiber.Static{
-		Compress:  true,
-		ByteRange: true,
-		// index default: index.html
-		CacheDuration: 60 * time.Second,
-		MaxAge:        60,
-	})
-
-	app.Use("*", func(ctx *fiber.Ctx) error {
-		if ctx.Method() != fiber.MethodGet {
-			return ctx.Next()
-		}
-
-		path := strings.TrimPrefix(ctx.Path(), "/")
-		base := filepath.Join(staticRoot, path)
-
-		for {
-			possibleIndex := filepath.Join(base, "index.html")
-			if _, err := os.Stat(possibleIndex); err == nil {
-				return ctx.SendFile(possibleIndex)
-			}
-
-			if base == staticRoot {
-				break
-			}
-
-			base = filepath.Dir(base)
-		}
-
-		defaultIndex := filepath.Join(staticRoot, "index.html")
-		if _, err := os.Stat(defaultIndex); err == nil {
-			return ctx.SendFile(defaultIndex)
-		}
-
-		return ctx.Next()
-	})
-
-	app.Use("*", func(ctx *fiber.Ctx) error {
-		log.Instance.Warn("Router not found", zap.String("ctx", ctx.String()))
-
-		return ctx.Status(fiber.StatusNotFound).JSON(response.New(false, "router not found"))
-	})
+	notfound.Setup("static not found", router)
 }
