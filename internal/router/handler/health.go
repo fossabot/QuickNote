81362f21d6f1cb64e268791055003bb8295fcd53
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-15 14:38:47 +0800

    feat: health route log

--- internal/router/handler/health.go
+++ internal/router/handler/health.go
@@ -1,12 +1,18 @@
 package handler
 
 import (
+	"github.com/Sn0wo2/QuickNote/pkg/common"
+	"github.com/Sn0wo2/QuickNote/pkg/log"
 	"github.com/Sn0wo2/QuickNote/pkg/response"
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
 func Health() func(ctx *fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		return c.JSON(response.New("success"))
+	return func(ctx *fiber.Ctx) error {
+		log.Instance.Info("Health check",
+			zap.String("ctx", common.FiberContextString(ctx)))
+
+		return ctx.JSON(response.New("success"))
 	}
 }
