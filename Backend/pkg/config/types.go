638c061c1c90183ae48e5ae25a3f3a3a7d7c406b
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-20 12:01:17 +0800

    feat(backend): config logger level

--- Backend/pkg/config/types.go
+++ Backend/pkg/config/types.go
@@ -1,6 +1,9 @@
 package config
 
 type Config struct {
+	Logger struct {
+		Level string `yaml:"level"`
+	} `yaml:"logger"`
 	Database struct {
 		Type string `yaml:"type"`
 		URL  string `yaml:"url"`
