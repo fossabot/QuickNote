6133cc95d5e1530d640c917cb69426ce1f862d51
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-09 21:32:00 +0800

    feat(updater): get download url
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- Updater/main.py
+++ Updater/main.py
@@ -1,16 +1,18 @@
 import json
 import time
+from pathlib import Path
 
 from Updater.config.yml_loader import get_config
+from Updater.updater.assets.name import get_package_name_from_current_machine
 from Updater.updater.runner.process import find_processes_by_path, try_terminate
 from Updater.updater.tag.reader import read
-from Updater.updater.tag.writer import write
 from fetch.github_release_api import fetch_latest_release
 from proxy.http import get_proxies
 
 OWNER = "Sn0wo2"
 REPO = "QuickNote"
 INTERVAL = 5.0
+
 
 def main():
     result = fetch_latest_release(OWNER, REPO, get_proxies())
@@ -24,16 +26,28 @@ def main():
         print("=" * 80)
         print(f"New tag: {tag}")
 
-        procs = find_processes_by_path(get_config().path.processPath)
+        procs = find_processes_by_path(Path(get_config().path.workPath, get_config().path.processName))
         if not procs:
             print("No matching processes found.")
         for proc in procs:
             try_terminate(proc)
 
-        write(tag)
+        download_url = None
+
+        for asset in result.get("assets"):
+            if asset.get("name") == get_package_name_from_current_machine():
+                download_url = asset.get("browser_download_url")
+                break
+
+        if download_url is None:
+            print(f"Could not find asset for {get_package_name_from_current_machine()}")
+            return
+
+        print(download_url)
 
     print("=" * 80)
     time.sleep(INTERVAL)
+
 
 if __name__ == "__main__":
     main()
