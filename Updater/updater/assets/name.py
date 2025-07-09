b6b2354fdaff4c1fd4190ac2844d48754237ac8f
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-09 21:23:40 +0800

    feat(updater): get package name
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

---
+++ Updater/updater/assets/name.py
@@ -0,0 +1,23 @@
+import platform
+
+
+def get_goos_goarch():
+    machine = platform.machine().lower()
+    if machine in ("x86_64", "amd64"):
+        goarch = "amd64"
+    elif machine in ("aarch64", "arm64"):
+        goarch = "arm64"
+    elif machine in ("i386", "i686", "x86"):
+        goarch = "386"
+    elif machine.startswith("arm"):
+        goarch = "arm"
+    else:
+        goarch = machine
+
+    return platform.system().lower(), goarch
+
+def get_package_name_from_current_machine():
+    goos, goarch = get_goos_goarch()
+
+    ext = ".zip" if goos == "windows" else ".tar.gz"
+    return f"QuickNote_{goos}_{goarch}{ext}"
