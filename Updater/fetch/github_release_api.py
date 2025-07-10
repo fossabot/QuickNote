779808b62f98d3e3465743441ba07a5dcbd6a3ee
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-10 21:03:11 +0800

    chore(updater): remove?
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- Updater/fetch/github_release_api.py
+++ Updater/fetch/github_release_api.py
@@ -1,12 +1,9 @@
 from typing import Optional, MutableMapping
 
 import requests
-from proxy.http import get_proxies
 
 
 def fetch_latest_release(owner: str, repo: str, proxies: Optional[MutableMapping[str, str]] = None) -> dict:
-    proxies = proxies or get_proxies()
-
     resp = requests.get(
         f"https://api.github.com/repos/{owner}/{repo}/releases/latest",
         headers={"Accept": "application/vnd.github.v3+json"},
